df434015e33a76d19cf6c5db410fa290148994ae
Benjamin Oakes <ben@benjaminoakes.com>
2011-08-07 20:47:20 -0400

    Moving scripts around and serving them up through node.js/express.js

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
-var express = require('express');
+var express = require('express'),
+    app = express.createServer(express.logger());
 
-var app = express.createServer(express.logger());
+app.use('/', express.static(__dirname + '/public/'));
+app.use('/vendor/', express.static(__dirname + '/vendor/'));
 
-app.get('/', function(request, response) {
-    response.send('Hello JavaScript World!');
-});
-
-var port = process.env.PORT || 3000;
-app.listen(port, function(){
-    console.log("Listening on " + port);
-});
+(function (port) {
+    app.listen(port, function () {
+        console.log("Listening on " + port);
+    });
+}(process.env.PORT || 3000));
