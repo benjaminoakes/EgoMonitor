81758b164c78b2def4793551439301fa1053a055
Benjamin Oakes <ben@benjaminoakes.com>
2011-08-09 20:31:30 -0400

    Rubygems.org API replayer to get around same-origin restriction

--- web.js
+++ web.js
@@ -1,9 +1,28 @@
 var express = require('express'),
-    app = express.createServer(express.logger());
+    http    = require('http');
+
+var app = express.createServer(express.logger());
 
 app.use('/', express.static(__dirname + '/public/'));
 app.use('/lib/', express.static(__dirname + '/lib/'));
 app.use('/vendor/', express.static(__dirname + '/vendor/'));
+
+app.get('/rubygems.json', function (request, response) {
+    var buffer = [];
+
+    // TODO this doesn't give back an object that's compatible with http.get... why?
+    // var opts = url.parse('http://rubygems.org/api/v1/versions/maid.json');
+
+    http.get({host: 'rubygems.org', path: '/api/v1/versions/maid.json'}, function (apiResponse) {
+        apiResponse.on('data', function (chunk) {
+            buffer.push(chunk);
+        }).on('end', function () {
+            response.send(buffer.join(''));
+        });
+    }).on('error', function (e) {
+        throw e;
+    });
+});
 
 (function (port) {
     app.listen(port, function () {
